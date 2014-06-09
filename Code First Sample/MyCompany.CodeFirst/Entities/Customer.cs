a72c24940f0f7a8fff70fdbe1cc3eb81d00b7f4e
Francisco Illanes <illanesfco@gmail.com>
2014-06-09 15:51:32 -0600

    Code First and Model First Samples

---
+++ Code First Sample/MyCompany.CodeFirst/Entities/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany.CodeFirst.Entities
+{
+    // This is a hand-crafted entity/class. We create/maintain these outselves in Code-First
+    public class Customer
+    {
+        // By default, EF will treat "CustomerId" as a Primary Key in the database and an Identity column in the database table
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Navigation Properties
+        public virtual IEnumerable<Order> Orders { get; set; }
+    }
+}
---
+++ Code First Sample/MyCompany.CodeFirst/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; // has attributes we can use for our Entity 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany.CodeFirst.Entities
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string ShipToAddress { get; set; }
+        public int CustomerId { get; set; }
+
+        // Navigation Properties
+        public virtual Customer Customer { get; set; }
+    }
+}
