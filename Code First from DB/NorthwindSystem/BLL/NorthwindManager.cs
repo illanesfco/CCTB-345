9eb708e0ba42624ca301f54f13f66595c1d7109c
Francisco Illanes <illanesfco@gmail.com>
2014-06-10 14:56:52 -0600

    Code First with Existing Database

---
+++ Code First from DB/NorthwindSystem/BLL/NorthwindManager.cs
@@ -0,0 +1,43 @@
+﻿using NorthwindSystem.DAL;
+using NorthwindSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity; // for some of the EF extension methods
+
+namespace NorthwindSystem.BLL
+{
+    // This is the primary public access into the NorthwindSystem's data 
+    public class NorthwindManager
+    {
+        public List<Employee> GetEmployees()
+        {
+            using (var context = new NWContext())
+            {
+                var result = context.Employees;
+                return result.ToList();
+            }
+        }
+
+        // TODO: Create a method called GetOrders that will return a list of Order objects from the DB
+        public List<Order> GetOrders()
+        {
+            using (var context = new NWContext())
+            {
+                var result = context.Orders;
+                return result.ToList();
+            }
+        }
+
+        public List<Region> GetRegions()
+        {
+            using (var context = new NWContext())
+            {
+                var result = context.Regions.Include(item => item.Territories).OrderBy(item => item.RegionDescription);
+                return result.ToList();
+            }
+        }
+    }
+}
