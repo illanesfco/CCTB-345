772adeadebe2f4fac95e7b27023c688a5d5b993a
Francisco Illanes <illanesfco@gmail.com>
2014-06-12 15:31:39 -0600

    ASP.NET and Basic File IO

---
+++ Code First from DB/NorthwindSystem/Entities/DTO/ShipperDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindSystem.Entities.DTO
+{
+    [Serializable]
+    public class ShipperDTO
+    {
+        public int ShipperID { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+  
+        public ShipperDTO()
+        {
+        }
+
+        public ShipperDTO(Shipper info)
+        {
+            ShipperID = info.ShipperID;
+            CompanyName = info.CompanyName;
+            Phone = info.Phone;
+        }
+
+        public Shipper ToShipper()
+        {
+            return new Shipper()
+            {
+                ShipperID = ShipperID,
+                CompanyName = CompanyName,
+                Phone = Phone
+            };
+        }
+    }
+}
